8a3eecb81b901db7691413d53a460d245361c221
Geeta Hyagadi <geeta.hyagadi@zebra.com>
2024-11-29 18:12:16 +0530

    swap numbers in array modified

--- FirstJava/src/SwapArray.java
+++ FirstJava/src/SwapArray.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class SwapArray {
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,5};
+        swap(arr,2,3);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void swap(int[] arr, int i, int i1) {
+        int temp = arr[i];
+        arr[i] = arr[i1];
+        arr[i1] = temp;
+    }
+
+
+
 }
